bc5f7d5b8f188cef9d2a01923d13d0c2c1539621
Rajiv Wallace <rajivghandi972@gmail.com>
2024-10-09 13:29:05 -0400

    update settings file for dev and prod

--- backend/portfolio_website/settings/local.py
+++ backend/portfolio_website/settings/local.py
@@ -1,1 +1,14 @@
 from .base import *
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = True
+
+
+# SQLite
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
