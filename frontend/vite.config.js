f5f60f6858d1bf875f4422c58b09edf341662c17
Rajiv Wallace <rajivghandi972@gmail.com>
2024-10-17 16:51:27 -0400

    deployment modifications

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,14 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
+  build: {
+    outDir: "dist",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console logs in production
+      },
+    },
   },
-  preview: {
-    host: true,
-    port: 4000,
-  },
+  base: "/", // Base URL for the application
 });
