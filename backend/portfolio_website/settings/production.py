a5bef8e9ec87aded2b75f9bd244ef6abf4ff6325
Rajiv Wallace <rajivghandi972@gmail.com>
2024-12-04 15:49:02 -0500

    update deployment settings

--- backend/portfolio_website/settings/production.py
+++ backend/portfolio_website/settings/production.py
@@ -2,11 +2,6 @@ from .base import *
 
 DEBUG = False
 
-STATICFILES_DIRS = [
-    # Replace "static" with the folder name where your static files are stored
-    BASE_DIR / "../staticfiles",
-]
-
 # PostgreSQL
 
 DATABASES = {
