719786efee6f6bf544fc3b1888606be8989b12ce
Rajiv Wallace <rajivghandi972@gmail.com>
2024-12-04 17:23:38 -0500

    update deployment settings

--- backend/portfolio_website/settings/base.py
+++ backend/portfolio_website/settings/base.py
@@ -85,10 +85,10 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, './staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '../mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, './mediafiles')
 
 # Default primary key field type
 
