fd3e384c5a10ec2290fc51bb4c0d59bfd04aa0f1
Rajiv Wallace <rajivghandi972@gmail.com>
2024-12-11 16:01:22 -0500

    update dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,16 @@
-# #!/bin/sh
+#!/bin/sh
 
-# # Exit on errors
-# set -e
+# Exit on errors
+set -e
 
-# # Ensure static and media directories exist
-# mkdir -p /django/staticfiles /django/mediafiles
+# Ensure static and media directories exist
+mkdir -p /staticfiles /mediafiles
 
-# # Run database migrations
-# python manage.py migrate --no-input
+# Run database migrations
+python manage.py migrate --no-input
 
-# # Collect static files
-# python manage.py collectstatic --no-input --clear
+# Collect static files
+python manage.py collectstatic --no-input --clear
 
-# # Start the main process
-# exec "$@"
+# Start the main process
+exec "$@"
