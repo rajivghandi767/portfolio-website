a937f12d52e8a65098a062438d067ba114f700c4
Rajiv Wallace <rajivghandi972@gmail.com>
2024-12-11 10:26:26 -0500

    update entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+mkdir -p /home/backend/django/staticfiles
+chown backend:backend_group /home/backend/django/staticfiles
+
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
